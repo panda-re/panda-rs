3326293a1a6c6d041181422e72764be2b7a1ce80
jamcleod <Jordan.McLeod@ll.mit.edu>
2020-08-13 11:24:23 -0400

    Add passing `-os` to panda options for example

--- run_example.sh
+++ run_example.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 cargo build && \
     cp target/debug/libpanda_rs_example.so $PANDA_PATH/x86_64-softmmu/panda/plugins/panda_panda_rs_example.so && \
-    $PANDA_PATH/x86_64-softmmu/panda-system-x86_64 -replay test -panda panda_rs_example -m 1G
+    $PANDA_PATH/x86_64-softmmu/panda-system-x86_64 -os "linux-64-ubuntu:4.15.0-72-generic-noaslr-nokaslr" -replay test -panda panda_rs_example -m 1G
