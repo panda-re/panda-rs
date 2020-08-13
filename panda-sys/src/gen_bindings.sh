c6d21a88002c0e3f690c530d76df80c8d7e4b9ae
jamcleod <Jordan.McLeod@ll.mit.edu>
2020-08-13 11:23:22 -0400

    Make binding generation more reproducible

--- panda-sys/src/gen_bindings.sh
+++ panda-sys/src/gen_bindings.sh
@@ -1,4 +1,4 @@
 #/bin/bash
-bindgen bindings.h -o bindings.rs --no-layout-tests -- -I/home/jordan/dev/panda/panda/include -I/home/jordan/dev/panda/build -I/home/jordan/dev/panda/build/x86_64-softmmu -I/home/jordan/dev/panda/include -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -I/home/jordan/dev/panda/target/i386 -I/home/jordan/dev/panda/tcg/i386 -I/home/jordan/dev/panda/tcg -DNEED_CPU_H -I/home/jordan/dev/panda
+bindgen bindings.h -o bindings.rs --no-layout-tests -- -I$PANDA_ROOT/panda/include -I$PANDA_ROOT/build -I$PANDA_ROOT/build/x86_64-softmmu -I$PANDA_ROOT/include -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -I$PANDA_ROOT/target/i386 -I$PANDA_ROOT/tcg/i386 -I$PANDA_ROOT/tcg -DNEED_CPU_H -I$PANDA_ROOT
 
 #TODO: make bindgen script use panda env variables and whatnot
