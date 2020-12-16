d437e78168d14fe45ad12508eb5fb6501cfce9e8
tnballo <tnballo@users.noreply.github.com>
2020-12-16 15:44:49 -0500

    Review fixes: FFI casting

--- panda-rs/src/api/mem.rs
+++ panda-rs/src/api/mem.rs
@@ -10,7 +10,7 @@ pub fn virtual_memory_read(cpu: &mut CPUState, addr: target_ulong, len: usize) -
 
     unsafe {
         let res = panda_sys::panda_virtual_memory_read_external(
-            &mut *cpu,
+            cpu,
             addr,
             buf.as_mut_ptr(),
             len as i32,
@@ -49,12 +49,12 @@ pub fn physical_memory_read(addr: target_ulong, len: usize) -> Result<Vec<u8>, M
 
 /// Write to guest virtual memory
 pub fn virtual_memory_write(cpu: &mut CPUState, addr: target_ulong, data: &[u8]) -> MemRWStatus {
-    let mut c_data = vec_u8_into_i8(data.to_vec()); // Alloc b/c C API wants mut
+    let mut c_data = data.to_vec(); // Alloc b/c C API wants mut
     unsafe {
         panda_sys::panda_virtual_memory_write_external(
-            &mut *cpu,
+            cpu,
             addr,
-            c_data.as_mut_ptr(),
+            c_data.as_mut_ptr() as *mut i8,
             c_data.len() as i32,
         ).into()
     }
@@ -76,7 +76,7 @@ pub fn physical_memory_write(addr: target_ulong, data: &[u8]) -> MemRWStatus {
 pub fn virt_to_phys(cpu: &mut CPUState, addr: target_ulong) -> target_ulong {
     unsafe {
         panda_sys::panda_virt_to_phys_external(
-            &mut *cpu,
+            cpu,
             addr,
         )
     }
@@ -97,19 +97,4 @@ fn vec_i8_into_u8(v: Vec<i8>) -> Vec<u8> {
 
     // Adopt the data into a new Vec
     unsafe { Vec::from_raw_parts(p as *mut u8, len, cap) }
-}
-
-// https://stackoverflow.com/questions/59707349/cast-vector-of-i8-to-vector-of-u8-in-rust/59707887#59707887
-// TODO: replace with https://doc.rust-lang.org/std/vec/struct.Vec.html#method.into_raw_parts, once on stable
-fn vec_u8_into_i8(v: Vec<u8>) -> Vec<i8> {
-    // Make sure v's destructor doesn't free the data it thinks it owns when it goes out of scope
-    let mut v = std::mem::ManuallyDrop::new(v);
-
-    // Pick apart the existing Vec
-    let p = v.as_mut_ptr();
-    let len = v.len();
-    let cap = v.capacity();
-
-    // Adopt the data into a new Vec
-    unsafe { Vec::from_raw_parts(p as *mut i8, len, cap) }
 }
--- panda-rs/src/api/misc.rs
+++ panda-rs/src/api/misc.rs
@@ -3,21 +3,21 @@ use crate::prelude::*;
 /// Determine if guest is currently in kernelspace
 pub fn in_kernel(cpu: &mut CPUState) -> bool {
     unsafe {
-        panda_sys::panda_in_kernel_external(&mut *cpu)
+        panda_sys::panda_in_kernel_external(cpu)
     }
 }
 
 /// Get current architecture independent Address-Space ID (ASID)
 pub fn current_asid(cpu: &mut CPUState) -> target_ulong {
     unsafe {
-        panda_sys::panda_current_asid(&mut *cpu)
+        panda_sys::panda_current_asid(cpu)
     }
 }
 
 /// Get current guest program counter
 pub fn current_pc(cpu: &mut CPUState) -> target_ulong {
     unsafe {
-        panda_sys::panda_current_pc(&mut *cpu)
+        panda_sys::panda_current_pc(cpu)
     }
 }
 
@@ -25,7 +25,7 @@ pub fn current_pc(cpu: &mut CPUState) -> target_ulong {
 /// Get current guest userspace stack pointer
 pub fn current_sp(cpu: &mut CPUState) -> target_ulong {
     unsafe {
-        panda_sys::panda_current_sp(&mut *cpu)
+        panda_sys::panda_current_sp(cpu)
     }
 }
 */
@@ -34,7 +34,7 @@ pub fn current_sp(cpu: &mut CPUState) -> target_ulong {
 /// Get current guest kernelspace stack pointer
 pub fn current_ksp(cpu: &mut CPUState) -> target_ulong {
     unsafe {
-        panda_sys::panda_current_ksp(&mut *cpu)
+        panda_sys::panda_current_ksp(cpu)
     }
 }
 */
@@ -43,7 +43,7 @@ pub fn current_ksp(cpu: &mut CPUState) -> target_ulong {
 /// Get current guest function return value
 pub fn get_ret_val(cpu: &mut CPUState) -> target_ulong {
     unsafe {
-        panda_sys::panda_ret_val(&mut *cpu)
+        panda_sys::panda_ret_val(cpu)
     }
 }
 */
