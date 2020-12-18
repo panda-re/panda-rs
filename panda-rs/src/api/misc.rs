461870e1bf042fcb47aebc40ee21387059513906
tnballo <tnballo@users.noreply.github.com>
2020-12-18 14:36:12 -0500

    Add a handful of misc APIs

--- panda-rs/src/api/misc.rs
+++ panda-rs/src/api/misc.rs
@@ -21,29 +21,46 @@ pub fn current_pc(cpu: &mut CPUState) -> target_ulong {
     }
 }
 
-/* TODO: expose in libpanda!
 /// Get current guest userspace stack pointer
 pub fn current_sp(cpu: &mut CPUState) -> target_ulong {
     unsafe {
-        panda_sys::panda_current_sp(cpu)
+        panda_sys::panda_current_sp_external(cpu)
     }
 }
-*/
 
-/* TODO: expose in libpanda!
+/// Get current guest userspace stack pointer, masking of page size MSBs
+pub fn current_sp_masked_pagesize(cpu: &mut CPUState, page_size: target_ulong) -> target_ulong {
+    unsafe {
+        panda_sys::panda_current_sp_masked_pagesize_external(cpu, page_size)
+    }
+}
+
 /// Get current guest kernelspace stack pointer
 pub fn current_ksp(cpu: &mut CPUState) -> target_ulong {
     unsafe {
-        panda_sys::panda_current_ksp(cpu)
+        panda_sys::panda_current_ksp_external(cpu)
     }
 }
-*/
 
-/* TODO: expose in libpanda!
 /// Get current guest function return value
 pub fn get_ret_val(cpu: &mut CPUState) -> target_ulong {
     unsafe {
-        panda_sys::panda_ret_val(cpu)
+        panda_sys::panda_get_retval_external(cpu)
     }
 }
-*/
+
+/// If required for the target architecture, enter into a high-privilege mode in order to conduct some memory access.
+/// Returns true if a switch into high-privilege mode has been made.
+/// A NO-OP on systems where such changes are unnecessary.
+pub fn enter_priv(cpu: &mut CPUState) -> bool {
+    unsafe {
+        panda_sys::enter_priv(cpu)
+    }
+}
+/// Revert the guest to the privilege mode it was in prior to the last call to enter_priv().
+/// A NO-OP for architectures where enter_priv() is a NO-OP.
+pub fn exit_priv(cpu: &mut CPUState) {
+    unsafe {
+        panda_sys::exit_priv(cpu)
+    }
+}
--- panda-rs/src/enums.rs
+++ panda-rs/src/enums.rs
@@ -1,3 +1,4 @@
+/// For fallible virt/phys memory R/W operations
 #[repr(i32)]
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
 pub enum MemRWStatus {
@@ -8,7 +9,6 @@ pub enum MemRWStatus {
     MemTxDecodeError = panda_sys::MEMTX_DECODE_ERROR as i32,
 }
 
-// TODO: tried https://crates.io/crates/enum-primitive-derive, didn't for this. Better way then below?
 impl From<i32> for MemRWStatus {
     fn from(v: i32) -> Self {
         match v {
@@ -17,6 +17,25 @@ impl From<i32> for MemRWStatus {
             x if x == MemRWStatus::MemTxError as i32 => MemRWStatus::MemTxError,
             x if x == MemRWStatus::MemTxDecodeError as i32 => MemRWStatus::MemTxDecodeError,
             _ => MemRWStatus::Unknown, // This means there is a bug in the C side of things
+        }
+    }
+}
+
+/// For fallible generic C functions
+#[repr(i32)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
+pub enum GenericRet {
+    Unknown = -2,
+    Error = -1,
+    Success = 0,
+}
+
+impl From<i32> for GenericRet {
+    fn from(v: i32) -> Self {
+        match v {
+            x if x == GenericRet::Error as i32 => GenericRet::Error,
+            x if x == GenericRet::Success as i32 => GenericRet::Success,
+            _ => GenericRet::Unknown, // This means there is a bug in the C side of things
         }
     }
 }
