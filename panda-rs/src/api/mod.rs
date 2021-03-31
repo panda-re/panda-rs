497dbb13cf551de57eb1a0f5e09308225324972b
jamcleod <Jordan.McLeod@ll.mit.edu>
2021-03-30 20:45:02 -0400

    Improve documentation

--- panda-rs/src/api/mod.rs
+++ panda-rs/src/api/mod.rs
@@ -1,6 +1,10 @@
+/// Functions for working with PANDA's LLVM execution
 pub mod llvm;
+/// Utilities for working with the guest's memory
 pub mod mem;
+/// Miscellaneous PANDA API utilities
 pub mod misc;
+/// Functions for reading and modifying guest registers
 pub mod regs;
 
 mod utils;
